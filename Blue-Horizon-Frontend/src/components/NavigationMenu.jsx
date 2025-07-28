be7ebe47f97b1d4fbdc93ac46a00e581f295a4d8
Bunheng30 <bunheng0306@gmail.com>
2025-07-28 12:59:58 +0700

    Done

--- Blue-Horizon-Frontend/src/components/NavigationMenu.jsx
+++ Blue-Horizon-Frontend/src/components/NavigationMenu.jsx
@@ -16,11 +16,7 @@ function NavigationMenu() {
 
 
       <div className="flex items-center gap-4">
-<<<<<<< HEAD
-        <Link to={user ? "/write" : "/login"} className="hover:text-blue-600 transition duration-200">
-=======
-        <Link to={user? "/write" : "/login"}>
->>>>>>> 512c5ab3083253659c6abb0e5686b151e59d1708
+        <Link to={user}>
           <button className="flex items-center gap-2 px-4 py-2 border border-gray-300 bg-white hover:bg-gray-100 hover:shadow-md transition-all duration-200">
             <PenTool size={18} />
             <span className="text-sm font-medium">Write</span>
